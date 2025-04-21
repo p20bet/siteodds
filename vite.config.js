23b25d63b2efc7ebbe44ab95ad0fc25efe45c1f5
p20bet <p20comercio@gmail.com>
2025-04-21 14:46:36 -0300

    Descrição das alterações realizadas

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-// vite.config.js
-export default {
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
   build: {
-    rollupOptions: {
-      external: ['axios']
-    }
-  }
-}
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+});
